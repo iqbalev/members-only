03e976a495113286d73344755085ca4186d40f30
Muhammad Iqbal <52034320+iqbalev@users.noreply.github.com>
2025-02-13 18:16:43 +0700

    chore: set up project

---
+++ app.js
@@ -0,0 +1,22 @@
+import { fileURLToPath } from "node:url";
+import path from "node:path";
+import express from "express";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => res.render("index", { heading: "Index" }));
+
+const PORT = 8080;
+const server = app.listen(PORT, () =>
+  console.log("Server running on http://localhost:8080")
+);
